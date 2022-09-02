d8676290d642487ee3caf31f27169eacce315198
acoss22 <anacseq@gmail.com>
2022-09-02 14:44:21 +0100

    feat: add landing page

---
+++ my-app/src/components/LandingPageComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LandingPage = () => {
+
+    return(
+    <div>
+        <button>
+            Add a sentence!
+        </button>
+    </div>
+    )
+
+
+
+}
+export default LandingPage;
